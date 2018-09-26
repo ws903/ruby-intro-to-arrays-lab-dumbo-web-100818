90e1489b4b67156befda7f1dc9133ab3f7fd4edb
David Sim <wsim903@gmail.com>
2018-09-26 18:49:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  lst = []
+  2.times do
+    lst.push(rand(1..10))
+  end
+  lst
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.first()
+end
+
+def last_element_with_array_methods(array)
+  array.last()
+end
+
+def length_of_array(array)
+  array.length()
+end
